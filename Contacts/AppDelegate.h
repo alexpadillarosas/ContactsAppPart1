50bd502f83cbdb85f45e26aa9d04d9a323289aae
kraken <alexpadillarosas@gmail.com>
2020-09-20 14:10:28 +1000

    Initial commit

---
+++ Contacts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Contacts
+//
+//  Created by alex on 20/9/20.
+//  Copyright © 2020 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
